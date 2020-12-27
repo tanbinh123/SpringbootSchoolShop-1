1ee155ae45f3f1be70603422b188ab7ae4f8bc1c
1780910546@qq.com <a13692540561>
2020-12-27 17:47:20 +0800

    Initial commit

---
+++ src/main/java/com/zhang/ssmschoolshop/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.zhang.ssmschoolshop.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(false)
+                .groupName("小张")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zhang.ssmschoolshop"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("毕设商城系统API")
+                .contact(new Contact("张锦林", "http://www.github.com/codingzhangxin", "994683607@qq.com"))
+                .version("v1.0")
+                .build();
+    }
+}
